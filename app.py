f4507da7759c77266934ad0272934b8a142bec20
RFCHINA\zengshaoyong <28310159@qq.com>
2020-01-14 09:13:45 +0800

    新增CMDB分支

--- app.py
+++ app.py
@@ -3,15 +3,6 @@ from app import app, api
 from gevent import monkey
 from gevent.pywsgi import WSGIServer
 from app.common.auth import Login, Logout
-from app.resources.deploy.manger import Manage
-from app.resources.deploy.deployips import Deployips
-from app.resources.deploy.uploads import Upload
-from app.resources.deploy.deployed_ips import Deployed_ips
-from app.resources.deploy.checkfile import Checkfile
-from app.resources.deploy.cpfile import Cpfile
-from app.resources.deploy.rmfile import Rmfile
-from app.resources.deploy.appips import App_ips
-from app.resources.deploy.touchfile import Touchfile
 
 monkey.patch_all()
 
@@ -20,15 +11,6 @@ server = WSGIServer(('0.0.0.0', 5000), app)
 api.add_resource(Mysql, '/mysql')
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
-api.add_resource(Manage, '/manage')
-api.add_resource(Deployips, '/deployips')
-api.add_resource(Deployed_ips, '/deployed_ips')
-api.add_resource(Upload, '/upload')
-api.add_resource(Checkfile, '/checkfile')
-api.add_resource(Cpfile, '/cp')
-api.add_resource(Rmfile, '/rm')
-api.add_resource(App_ips, '/app_ips')
-api.add_resource(Touchfile, '/touch')
 
 if __name__ == '__main__':
     server.serve_forever()
--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import flask_restful
 from flask_login import LoginManager
-from flask_cache import Cache
+# from flask_cache import Cache
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_restful import Api
@@ -13,7 +13,6 @@ from flask_cors import CORS
 from app.common.abort import my_abort
 
 login_manager = LoginManager()
-cache = Cache(config={'CACHE_TYPE': 'simple'})
 
 app = Flask(__name__)
 api = Api(app, errors=errors)
@@ -22,13 +21,12 @@ app.config['SECRET_KEY'] = configs[APP_ENV].SECRET_KEY
 app.permanent_session_lifetime = timedelta(minutes=10)
 login_manager.init_app(app)
 login_manager.session_protection = 'strong'
-cache.init_app(app)
+# cache.init_app(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = configs[APP_ENV].SQLALCHEMY_DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_COMMIT_TEARDOWN = True
 db = SQLAlchemy(app)
 flask_restful.abort = my_abort
-
 
 limiter = Limiter(
     app,
